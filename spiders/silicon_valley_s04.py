09084b20f30da38277bdc58d8cbcba1777149e59
wizdown <abhishekgupta.my@gmail.com>
2017-06-12 14:20:22 +0530

    spiders modified

--- spiders/silicon_valley_s04.py
+++ spiders/silicon_valley_s04.py
@@ -17,27 +17,26 @@ def extract_new_data(url):
     return list
 
 
-def extract_originals_data():
+def extract_silicon_valley_data():
     base_url = 'http://fmovie.io'
-    originals_data = extract_data('http://fmovie.io/search.html?keyword=originals')
-    if( len(originals_data) < 4):
+    silicon_valley_data = extract_data('http://fmovie.io/search.html?keyword=silicon+valley')
+    if( len(silicon_valley_data) == 0):
         return 0
-    
-    data = originals_data[3]
+    data =silicon_valley_data[4]
     data = data.find('a')
     data = data['href']
     new_url = base_url + data
     # print new_url
-    new_originals_data = extract_new_data(new_url)
-    if( len(new_originals_data) == 0):
+    new_silicon_valley_data = extract_new_data(new_url)
+    if( len(new_silicon_valley_data) == 0):
         return 0
-    new_originals_data = new_originals_data[0]
-    data = new_originals_data.text
+    new_silicon_valley_data = new_silicon_valley_data[0]
+    data = new_silicon_valley_data.text
     index = data.find('Episode')
     index = index + 8
     episode_no = data[index:index+2]
     return episode_no
 
-new_episode_no = extract_originals_data()
-info = 'originals_s04:' + str(new_episode_no)
+new_episode_no = extract_silicon_valley_data()
+info = 'silicon_valley_s04:' + str(new_episode_no)
 print info
